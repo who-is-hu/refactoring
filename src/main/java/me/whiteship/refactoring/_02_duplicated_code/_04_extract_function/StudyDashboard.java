f7944d6fb59cb14ce8e81f570bd1650db0bee06b
who-is-hu <dydgndlaek97@gmail.com>
2022-04-01 01:37:05 +0900

    냄새2 - 중복코드

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
@@ -10,32 +10,35 @@ import java.util.Set;
 
 public class StudyDashboard {
 
+    // 주석이 사라지고 세줄로 읽히는 마법
+    // 서비스 로직의 코드는 이런 모양을 가지고 구현은 도메인 객체로 숨겨야 한다고 나는 생각한다.
     private void printParticipants(int eventId) throws IOException {
-        // Get github issue to check homework
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(eventId);
+        GHIssue issue = getGitHubIssue(eventId);
+        Set<String> participants = getParticipants(issue);
+        print(participants);
+    }
 
-        // Get participants
-        Set<String> participants = new HashSet<>();
-        issue.getComments().forEach(c -> participants.add(c.getUserName()));
-
-        // Print participants
+    private void print(Set<String> participants) {
         participants.forEach(System.out::println);
     }
 
-    private void printReviewers() throws IOException {
-        // Get github issue to check reviews
+    private Set<String> getParticipants(GHIssue issue) throws IOException {
+        Set<String> participants = new HashSet<>();
+        issue.getComments().forEach(c -> participants.add(c.getUserName()));
+        return participants;
+    }
+
+    private GHIssue getGitHubIssue(int eventId) throws IOException {
         GitHub gitHub = GitHub.connect();
         GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(30);
+        GHIssue issue = repository.getIssue(eventId);
+        return issue;
+    }
 
-        // Get reviewers
-        Set<String> reviewers = new HashSet<>();
-        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
-
-        // Print reviewers
-        reviewers.forEach(System.out::println);
+    private void printReviewers() throws IOException {
+        GHIssue issue = getGitHubIssue(30);
+        Set<String> reviewers = getParticipants(issue);
+        print(reviewers);
     }
 
     public static void main(String[] args) throws IOException {
