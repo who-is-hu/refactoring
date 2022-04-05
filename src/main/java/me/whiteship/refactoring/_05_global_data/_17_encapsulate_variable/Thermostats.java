df1ecbbaa0f8ea080fe8ab0553f2a7b564e22c99
who-is-hu <dydgndlaek97@gmail.com>
2022-04-06 00:59:06 +0900

    냄새5 - 전역변수

--- src/main/java/me/whiteship/refactoring/_05_global_data/_17_encapsulate_variable/Thermostats.java
+++ src/main/java/me/whiteship/refactoring/_05_global_data/_17_encapsulate_variable/Thermostats.java
@@ -2,9 +2,41 @@ package me.whiteship.refactoring._05_global_data._17_encapsulate_variable;
 
 public class Thermostats {
 
-    public static Integer targetTemperature = 70;
+    private static Integer targetTemperature = 70;
 
-    public static Boolean heating = true;
+    private static Boolean heating = true;
+
+    private static Integer getTargetTemperature() {
+        return targetTemperature;
+    }
+
+    public static void setTargetTemperature(Integer targetTemperature) {
+        Thermostats.targetTemperature = targetTemperature;
+    }
+
+    public static Boolean getHeating() {
+        return heating;
+    }
+
+    public static void setHeating(Boolean heating) {
+        Thermostats.heating = heating;
+    }
+
+    public static Boolean getCooling() {
+        return cooling;
+    }
+
+    public static void setCooling(Boolean cooling) {
+        Thermostats.cooling = cooling;
+    }
+
+    public static Boolean getReadInFahrenheit() {
+        return readInFahrenheit;
+    }
+
+    public static void setReadInFahrenheit(Boolean readInFahrenheit) {
+        Thermostats.readInFahrenheit = readInFahrenheit;
+    }
 
     public static Boolean cooling = false;
 
