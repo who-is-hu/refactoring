e559bf14886a16ae8fe30f050a5b74a60ac85b09
who-is-hu <dydgndlaek97@gmail.com>
2022-05-29 14:54:41 +0900

    냄새15 - 추측성 일반화

--- src/test/java/me/whiteship/refactoring/_15_speculative_generality/_35_remove_dead_code/ReservationTest.java
+++ src/test/java/me/whiteship/refactoring/_15_speculative_generality/_35_remove_dead_code/ReservationTest.java
@@ -11,9 +11,7 @@ class ReservationTest {
     @Test
     void reservation() {
         Reservation reservation = new Reservation(
-                "tennis",
-                LocalDateTime.of(2022, 1, 20, 19, 30),
-                LocalDateTime.of(2022, 1, 20, 9, 0));
+                LocalDateTime.of(2022, 1, 20, 19, 30));
         reservation.setAlarmBefore(30);
         assertEquals(LocalDateTime.of(2022, 1, 20, 19, 0), reservation.getAlarm());
     }
