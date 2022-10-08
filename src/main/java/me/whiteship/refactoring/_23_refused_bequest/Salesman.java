90ebe213b750ad9056baf585f56ef8f405809819
who-is-hu <dydgndlaek97@gmail.com>
2022-10-08 21:53:58 +0900

    냄새23 - 상속포기

--- src/main/java/me/whiteship/refactoring/_23_refused_bequest/Salesman.java
+++ src/main/java/me/whiteship/refactoring/_23_refused_bequest/Salesman.java
@@ -2,5 +2,10 @@ package me.whiteship.refactoring._23_refused_bequest;
 
 public class Salesman extends Employee {
 
+    private Quota quota;
+
+    public Quota getQuota() {
+        return new Quota();
+    }
 }
 
